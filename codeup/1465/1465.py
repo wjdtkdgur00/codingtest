5137569f2cd95d560cc689aaea48055dd79b0072
wjdtkdgur00 <wjdtkdgur1218@gmail.com>
2025-06-09 17:43:00 +0900

    codingtest

---
+++ codeup/1465/1465.py
@@ -0,0 +1,13 @@
+n, m = input().split()
+n,m = int(n), int(m)
+arr = [[0 for i in range(m)] for j in range(n)]
+k = 1
+for i in range(n - 1, -1, -1):
+    for j in range(m):
+        arr[i][j] = k
+        k += 1
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=' ')
+    print()
---
+++ codeup/1466/1466.py
@@ -0,0 +1,17 @@
+n, m = input().split()
+n, m = int(n), int(m)
+
+arr = [[0 for i in range(m)] for j in range(n)]
+
+k = n * m
+
+for j in range(m):
+    for i in range(n):
+        arr[i][j] = k
+        k -=1
+
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=' ')
+    print()
