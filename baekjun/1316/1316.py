c5ccf60027d2dc74a4f58eb299c640e386ff833d
wjdtkdgur00 <wjdtkdgur1218@gmail.com>
2025-06-18 09:08:20 +0900

    solve1316

--- baekjun/1316/1316.py
+++ baekjun/1316/1316.py
@@ -1,17 +1,15 @@
 n = int(input())
-
-arr = [input() for i in range(n)]
-cmp = ''
-check = 0
 cnt = 0
 for i in range(n):
-    for j in range(len(arr[i])):
-        if cmp == arr[i][j]:
-            check = 0
+    word = input()
+    check = []
+    for j in range(len(word)):
+        if j > 0 and word[j] != word[j - 1] and word[j] in check:
+            word = 'NO'
             break
-        else:
-            cmp = arr[i][j]
-            check = 1
-    if check == 1:
+        if word[j] not in check:
+            check.append(word[j])
+    if word != 'NO':
         cnt += 1
-print(cnt)
+
+print(cnt)        
